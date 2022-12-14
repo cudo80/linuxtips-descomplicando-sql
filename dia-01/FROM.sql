96bf913a93e5e5281ee90b6043f1401c9dca50d4
cudo80 <cudo80>
2022-12-14 00:06:48 +0000

    comandos-select-from

---
+++ dia-01/FROM.sql
@@ -0,0 +1,31 @@
+-- Databricks notebook source
+SELECT
+  *
+FROM
+  silver_olist.pedido -- leia-se: selecione TUDO da tabela silver_olist.pedido
+
+-- COMMAND ----------
+
+SELECT
+  idPedido
+FROM
+  silver_olist.pedido -- leia-se: selecione a coluna idPedido da tabela silver_olist.pedido
+
+-- COMMAND ----------
+
+SELECT idPedido,
+       descSituacao
+       
+FROM silver_olist.pedido
+
+-- leia-se: selecione as colunas idPedido e descSituacao da tabela silver_olist.pedido
+
+-- COMMAND ----------
+
+SELECT
+  idPedido,
+  descSituacao
+FROM
+  silver_olist.pedido
+LIMIT
+  5 -- leia-se: selecione as colunas idPedido e descSituacao da tabela silver_olist.pedido
